0fb5ee4ceb9d9b2863369084f5c08cb1f7979fb2
milanthanujaguna <milanthanuja@gmail.com>
2020-05-08 18:37:24 +0800

    add depends

--- cooktest/recipes/remove.rb
+++ cooktest/recipes/remove.rb
@@ -2,9 +2,9 @@ dep_gems = [ { "gem" => "aws-sdk", "version" => "3" }]
 
 dep_gems.each do |gem|
   chef_gem gem["gem"] do
-    compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
+    #compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
     action :remove
-    options("--no-ri --no-rdoc")
+    #options("--no-ri --no-rdoc")
     version gem["version"]
   end
 end
