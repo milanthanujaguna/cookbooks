760022777d05d69ee69ff4b14c10717f805442c7
milanthanujaguna <milanthanuja@gmail.com>
2020-05-08 14:08:50 +0800

    new file

---
+++ cooktest/recipes/getec2id.rb
@@ -0,0 +1,8 @@
+require 'aws-sdk'
+ec2 = Aws::EC2::Client.new(region: 'us-east-1')
+describe_instances_result = ec2.describe_instances
+bash 'dummyfile' do
+    code <<-EOH
+        echo "#{describe_instances_result}" >> /root/test.txt
+        EOH
+end
--- cooktest/recipes/testing.rb
+++ cooktest/recipes/testing.rb
@@ -1,14 +1,19 @@
+#update system
+execute "upddate" do
+  command "yum update -y"
+  action :run
+end
+#install httpd
 package 'httpd' do
   action :upgrade
 end
-
 # Start docker service
 service 'httpd' do
   action [:enable, :start]
 end
-
+#add a file to root
 bash 'dummyfile' do
   code <<-EOH
-    echo "CREATEME" >> /home/root/test.txt
+    echo "CREATEME" >> /root/test.txt
     EOH
 end
