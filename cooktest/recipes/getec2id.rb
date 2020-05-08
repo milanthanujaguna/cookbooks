cd18ce40c026bf23c7018e76df916859f61ae1da
milanthanujaguna <milanthanuja@gmail.com>
2020-05-08 19:26:55 +0800

    add depends

--- cooktest/recipes/getec2id.rb
+++ cooktest/recipes/getec2id.rb
@@ -3,6 +3,14 @@ require 'aws-sdk-ec2'
 ec2 = Aws::EC2::Client.new(region: 'us-east-1')
 describe_instances_result = ec2.describe_instances
 puts describe_instances_result
+describe_instances_result.reservations.each do |reservation|
+  if reservation.instances.count > 0
+    reservation.instances.each do |instance|
+      puts "=" * (instance.instance_id.length + 13)
+      puts "Instance ID: #{instance.instance_id}"
+  end
+end
+
 #bash 'dummyfile' do
 #    code <<-EOH
 #        echo "#{describe_instances_result}" >> /root/test.txt
