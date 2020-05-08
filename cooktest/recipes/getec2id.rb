b505b7126c4cd3aaa1c2df405c8cb028ed2a4eb3
milanthanujaguna <milanthanuja@gmail.com>
2020-05-08 18:13:27 +0800

    add depends

--- cooktest/recipes/getec2id.rb
+++ cooktest/recipes/getec2id.rb
@@ -2,8 +2,9 @@ include_recipe 'cooktest::depends'
 require 'aws-sdk-ec2'
 ec2 = Aws::EC2::Client.new(region: 'us-east-1')
 describe_instances_result = ec2.describe_instances
-bash 'dummyfile' do
-    code <<-EOH
-        echo "#{describe_instances_result}" >> /root/test.txt
-        EOH
-end
+puts describe_instances_result
+#bash 'dummyfile' do
+#    code <<-EOH
+#        echo "#{describe_instances_result}" >> /root/test.txt
+#        EOH
+#end
---
+++ cooktest/recipes/remove.rb
@@ -0,0 +1,10 @@
+dep_gems = [ { "gem" => "aws-sdk-ec2", "version" => "3" }]
+
+dep_gems.each do |gem|
+  chef_gem gem["gem"] do
+    compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
+    action :remove
+    options("--no-ri --no-rdoc")
+    version gem["version"]
+  end
+end
